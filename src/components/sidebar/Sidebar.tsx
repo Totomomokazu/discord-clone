5003337fe536ac7ac28c7532d4d7429af722c71c
Totomomokazu <kanketomokazu@gmail.com>
2024-04-19 22:21:22 +0900

    commit

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Sidebar.sass"
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        {/* sidebarLeft */}
+        <div className='sidebarLeft'></div>
+
+
+        {/* sidebarRight */}
+        <div className='sidebarRight'></div>
+    </div>
+  )
+}
+
+export default Sidebar
