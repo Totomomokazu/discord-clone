a9056cec3d74d03118900e51370045ed7cc0350c
Totomomokazu <kanketomokazu@gmail.com>
2024-04-19 23:06:32 +0900

    commit

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import "./Sidebar.sass"
+import "./Sidebar.scss"
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         {/* sidebarLeft */}
-        <div className='sidebarLeft'></div>
-
-
+        <div className='sidebarLeft'>
+            <div className='sidebarIcon'>
+                <img src="./logo192.png" alt="" />
+            </div>
+            <div className='sidebarIcon'>
+                <img src="./logo192.png" alt="" />
+            </div>   
+        </div>
+   
+   
         {/* sidebarRight */}
-        <div className='sidebarRight'></div>
+        <div className='sidebarRight'>
+            <div className='sidebarTop'>
+                <h3>discord</h3>
+            </div>
+        </div>
     </div>
   )
 }
