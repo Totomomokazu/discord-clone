7cef1f9bddd3203ee4b96f13f8558f87d369d1f5
Totomomokazu <kanketomokazu@gmail.com>
2024-04-20 21:41:14 +0900

    commit

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import "./Sidebar.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import SidebarChannel from './SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 
 const Sidebar = () => {
@@ -38,6 +41,25 @@ const Sidebar = () => {
 
                 <div className='sidebarChannelList'>
                     <SidebarChannel/>
+                    <SidebarChannel/>
+                    <SidebarChannel/>
+                    <SidebarChannel/>
+                </div>
+
+
+                <div className='sidebarFooter'>
+                    <div className='sidebarAccount'> 
+                        <img src="./icon.jpg" alt="" />
+                        <div className='accountName'>
+                            <h4>Tomokazu</h4>
+                        </div>                  
+                    </div>
+                    <div className='sidebarVoice'>
+                        <MicIcon/>
+                        <HeadphonesIcon/>
+                        <SettingsIcon/>
+                    </div>
+
 
                 </div>
             </div>
